4d4acccfe222e053d2b3d89131814c858d701846
alexsincai <alex.sincai@yahoo.co.uk>
2022-07-22 14:50:49 +0300

    js refactored

--- static/bonusQuestions.js
+++ static/bonusQuestions.js
@@ -1,69 +1,51 @@
-// you receive an array of objects which you must sort in the by the key "sortField" in the "sortDirection"
 function getSortedItems(items, sortField, sortDirection) {
-    // console.log(items)
-    // console.log(sortField)
-    // console.log(sortDirection)
-
-    // === SAMPLE CODE ===
-    // if you have not changed the original html uncomment the code below to have an idea of the
-    // effect this function has on the table
-    //
     if (sortDirection === "asc") {
-        const firstItem = items.shift()
+        const firstItem = items.shift();
         if (firstItem) {
-            items.push(firstItem)
+            items.push(firstItem);
         }
     } else {
-        const lastItem = items.pop()
+        const lastItem = items.pop();
         if (lastItem) {
-            items.push(lastItem)
+            items.push(lastItem);
         }
     }
 
-    return items
+    return items;
 }
 
-// you receive an array of objects which you must filter by all it's keys to have a value matching "filterValue"
 function getFilteredItems(items, filterValue) {
-    // console.log(items)
-    // console.log(filterValue)
-
-    // === SAMPLE CODE ===
-    // if you have not changed the original html uncomment the code below to have an idea of the
-    // effect this function has on the table
-
-    let result = []
-    for (let i=0; i<items.length; i++) {
-        if (filterValue.startsWith("!Description:")) {
-            if (!items[i].Description.includes(filterValue.substring("!Description:".length))) {
-                result.push(items[i])
-            }
-        } else if (filterValue.startsWith("Description:")){
-            if (items[i].Description.includes(filterValue.substring("Description:".length))) {
-                result.push(items[i])
-            }
-        } else if (filterValue[0] === "!") {
-            if (!items[i].Title.includes(filterValue.slice(1))) {
-                result.push(items[i])
-            }
-        } else if (items[i].Title.includes(filterValue)) {
-            result.push(items[i])
+    let result = [];
+    for (let i = 0; i < items.length; i++) {
+        if (
+            (filterValue.startsWith("!Description:") &&
+                !items[i].Description.includes(
+                    filterValue.substring("!Description:".length)
+                )) ||
+            (filterValue.startsWith("Description:") &&
+                items[i].Description.includes(
+                    filterValue.substring("Description:".length)
+                )) ||
+            (filterValue.startsWith("Title:") &&
+                items[i].Title.includes(
+                    filterValue.substring("Title:".length)
+                )) ||
+            (filterValue.startsWith("!Title:") &&
+                !items[i].Title.includes(
+                    filterValue.substring("!Title:".length)
+                ))
+        ) {
+            result.push(items[i]);
         }
     }
-    return result
+    return result;
 }
 
 function toggleTheme() {
     document.body.classList.toggle("dark-mode");
-    // console.log("toggle theme")
 }
-document.querySelector("#theme-button").addEventListener("click", toggleTheme)
+document.querySelector("#theme-button").addEventListener("click", toggleTheme);
 
+function increaseFont() {}
 
-function increaseFont() {
-    // console.log("increaseFont")
-}
-
-function decreaseFont() {
-    // console.log("decreaseFont")
-}
+function decreaseFont() {}
--- static/script.js
+++ static/script.js
@@ -1,11 +1,19 @@
-const verifyButton = document.querySelectorAll(".verify")
+const verifyButton = document.querySelectorAll(".verify");
 
 async function toggleButton(event, element) {
-    event.preventDefault()
-    const request = await fetch(event.target.action);
+    event.preventDefault();
+    const request = await fetch(event.target.action, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            bila: event.target.dataset.bila //  <button data-bila="alba">Bila alba</button>
+        })
+        //  if request.method == "POST":
+        //      bila = request.json.get("bila")
+    });
     if (request.ok) {
-        const response = await request.json()
-        
+        const response = await request.json();
+
         element.querySelector("button i").classList.toggle("bi-app");
         element.querySelector("button i").classList.toggle("bi-check2-square");
     }
@@ -13,8 +21,6 @@ async function toggleButton(event, element) {
 
 verifyButton.forEach(function (element) {
     element.addEventListener("submit", async (event) => {
-        await toggleButton(event, element)
-    })
-})
-
-
+        await toggleButton(event, element);
+    });
+});
